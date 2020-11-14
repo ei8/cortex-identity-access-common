3f10d28639b00ff114050424685622a975e47b9a
elmerbool <35117496+elmerbool@users.noreply.github.com>
2020-11-14 19:03:36 +1100

    Fixed ErrorInfo JsonConstructor.

--- src/main/ErrorInfo.cs
+++ src/main/ErrorInfo.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -10,6 +11,7 @@ namespace ei8.Cortex.IdentityAccess.Common
         {
         }
 
+        [JsonConstructor]
         public ErrorInfo(string description, ErrorType type)
         {
             this.Description = description;
